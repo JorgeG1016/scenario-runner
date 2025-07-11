0556f84dd722d069c9ef18a60a13e9fd060b2498
Jorge Gomez <jgomez101609@gmail.com>
2025-07-10 20:19:20 -0400

    adding more config options and unit tests

--- src/config.rs
+++ src/config.rs
@@ -15,9 +15,20 @@ pub enum ConfigErrors {
 }
 
 #[derive(Deserialize, Debug, PartialEq)]
+#[serde(tag = "type")]
+pub enum ConfigIOType {
+    USB { port: String, baud_rate: u32 },
+    TCP { address: String },
+}
+
+#[derive(Deserialize, Debug, PartialEq)]
+#[serde(deny_unknown_fields)]
 pub struct Config {
     commands_path: PathBuf,
-    results_path: PathBuf,
+    #[serde(default)]
+    results_path: Option<PathBuf>,
+    sequence: Vec<PathBuf>,
+    io_interface: ConfigIOType,
 }
 
 impl Config {
@@ -28,8 +39,12 @@ impl Config {
 
         let config_file = File::open(config_file_path)?;
         let config_reader = io::BufReader::new(config_file);
-        let parsed_config: Config = serde_json::from_reader(config_reader)?;
-
+        let mut parsed_config: Config = serde_json::from_reader(config_reader)?;
+        if parsed_config.results_path.is_none() {
+            let mut temp_path = parsed_config.commands_path.clone();
+            temp_path.push("results");
+            parsed_config.results_path = Some(temp_path);
+        }
         Ok(parsed_config)
     }
 }
@@ -83,7 +98,7 @@ mod tests {
     }
 
     #[test]
-    fn config_fail_missing_required_field() {
+    fn config_new_fail_missing_required_field() {
         let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
         let raw_json = r#"
             {
@@ -103,12 +118,65 @@ mod tests {
     }
 
     #[test]
-    fn config_new_pass() {
+    fn config_new_fail_unknown_field_present() {
         let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
         let raw_json = r#"
             {
                 "commands_path": ".",
-                "results_path": "."
+                "results_path": ".",
+                "sequence": ["test.txt", "test.txt"],
+                "unknown_field": "."
+            }
+            "#;
+        temp_file
+            .write_all(raw_json.as_bytes())
+            .expect("Failed to write to temp file");
+
+        let result = Config::new(temp_file.path().to_path_buf());
+
+        assert!(
+            result.is_err(),
+            "Somehow there was valid json in this temp file"
+        );
+    }
+
+    #[test]
+    fn config_new_fail_io_interface_field_mismatch() {
+        let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
+        let raw_json = r#"
+            {
+                "commands_path": ".",
+                "results_path": ".",
+                "sequence": ["test1.txt", "test2.txt"],
+                "io_interface": {
+                    "type": "USB",
+                    "address": "test:test"
+                }
+            }
+            "#;
+        temp_file
+            .write_all(raw_json.as_bytes())
+            .expect("Failed to write to temp file");
+
+        let result = Config::new(temp_file.path().to_path_buf());
+
+        assert!(
+            result.is_err(),
+            "Somehow there was valid json in this temp file"
+        );
+    }
+
+    #[test]
+    fn config_new_pass_without_results_path() {
+        let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
+        let raw_json = r#"
+            {
+                "commands_path": ".",
+                "sequence": ["test1.txt", "test2.txt"],
+                "io_interface": {
+                    "type": "TCP",
+                    "address": "test:test"
+                }
             }
             "#;
         temp_file
@@ -118,7 +186,73 @@ mod tests {
             Config::new(temp_file.path().to_path_buf()).expect("Failed to create struct somehow");
         let assert_config = Config {
             commands_path: PathBuf::from("."),
-            results_path: PathBuf::from("."),
+            results_path: Some(PathBuf::from(".").join("results")),
+            sequence: vec![PathBuf::from("test1.txt"), PathBuf::from("test2.txt")],
+            io_interface: ConfigIOType::TCP {
+                address: String::from("test:test"),
+            },
+        };
+        assert_eq!(result, assert_config);
+    }
+
+    #[test]
+    fn config_new_pass_tcp_io() {
+        let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
+        let raw_json = r#"
+            {
+                "commands_path": ".",
+                "results_path": ".",
+                "sequence": ["test1.txt", "test2.txt"],
+                "io_interface": {
+                    "type": "TCP",
+                    "address": "test:test"
+                }
+            }
+            "#;
+        temp_file
+            .write_all(raw_json.as_bytes())
+            .expect("Failed to write to temp file");
+        let result =
+            Config::new(temp_file.path().to_path_buf()).expect("Failed to create struct somehow");
+        let assert_config = Config {
+            commands_path: PathBuf::from("."),
+            results_path: Some(PathBuf::from(".")),
+            sequence: vec![PathBuf::from("test1.txt"), PathBuf::from("test2.txt")],
+            io_interface: ConfigIOType::TCP {
+                address: String::from("test:test"),
+            },
+        };
+        assert_eq!(result, assert_config);
+    }
+
+    #[test]
+    fn config_new_pass_usb_io() {
+        let mut temp_file = NamedTempFile::new().expect("Failed to create temp file");
+        let raw_json = r#"
+            {
+                "commands_path": ".",
+                "results_path": ".",
+                "sequence": ["test1.txt", "test2.txt"],
+                "io_interface": {
+                    "type": "USB",
+                    "port": "test",
+                    "baud_rate": 115200
+                }
+            }
+            "#;
+        temp_file
+            .write_all(raw_json.as_bytes())
+            .expect("Failed to write to temp file");
+        let result =
+            Config::new(temp_file.path().to_path_buf()).expect("Failed to create struct somehow");
+        let assert_config = Config {
+            commands_path: PathBuf::from("."),
+            results_path: Some(PathBuf::from(".")),
+            sequence: vec![PathBuf::from("test1.txt"), PathBuf::from("test2.txt")],
+            io_interface: ConfigIOType::USB {
+                port: String::from("test"),
+                baud_rate: 115200,
+            },
         };
         assert_eq!(result, assert_config);
     }
