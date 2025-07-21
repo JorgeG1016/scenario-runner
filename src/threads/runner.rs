c9928dc7de6c30925749b5638a2e986b648d28d7
Jorge Gomez <jgomez101609@gmail.com>
2025-07-20 20:56:06 -0400

    added simple connection read to runner thread

--- src/threads/runner.rs
+++ src/threads/runner.rs
@@ -1,4 +1,7 @@
 use crate::connection::Communicate;
 
-
-pub fn thread(connection_handle: Box<dyn Communicate + Send + 'static>){}
+pub fn thread(connection_handle: &mut Box<dyn Communicate + Send + 'static>) {
+    let mut buf: [u8; 100] = [0; 100];
+    let result = connection_handle.read_until(&mut buf, b'\n');
+    print!("{:?}", buf);
+}
