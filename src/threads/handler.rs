cabb5d3d8e65b630106c3eb6dcb7bb679730bd61
Jorge Gomez <jgomez101609@gmail.com>
2025-07-22 23:01:50 -0400

    began adding command timeout logic

--- src/threads/handler.rs
+++ src/threads/handler.rs
@@ -1,8 +1,9 @@
-use crate::interaction::command::{self, Command, parse_scenario};
+use crate::interaction::command::{self, parse_scenario, Command, Sendable};
 use crate::interaction::config::Config;
 use super::itc::{Itc, Messages};
 use log::{info, warn};
 use std::time::Instant;
+use std::thread;
 
 pub fn thread(config: Config, runner_channels: Itc) {
     info!("Starting Scenario Handler Thread");
@@ -28,7 +29,33 @@ pub fn thread(config: Config, runner_channels: Itc) {
                     expect_exact,
                     timeout,
                     delay,
-                } => {}
+                } => {
+                    let data = match send {
+                        Sendable::Hex { data } => data,
+                        Sendable::Text { data } => data,
+                    };
+                    thread::sleep(delay);
+                    runner_channels.send_channel.send(Messages::SendData { data: data });
+                    let start_time = Instant::now();
+                    while Instant::now() - start_time < timeout {
+                        let received_data = runner_channels.receive_channel.recv();
+                        
+                    }
+                },
+                command::Type::Wait { 
+                    expect_prefix, 
+                    expect_exact, 
+                    timeout, 
+                    delay 
+                } => {
+
+                },
+                command::Type::WriteOnly { 
+                    send, 
+                    delay 
+                } => {
+
+                }
             };
         }
     }
