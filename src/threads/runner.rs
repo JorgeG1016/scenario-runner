915558ba9b85b6a1955326e200eacfe24aca0f64
Jorge Gomez <jgomez101609@gmail.com>
2025-07-22 20:47:08 -0400

    adding channels as argument to runner thread

--- src/threads/runner.rs
+++ src/threads/runner.rs
@@ -1,7 +1,7 @@
-use crate::connection::Communicate;
+use crate::{connection::Communicate, threads::Itc};
 use log::info;
 
-pub fn thread(connection_handle: &mut Box<dyn Communicate + Send + 'static>) {
+pub fn thread(connection_handle: &mut Box<dyn Communicate + Send + 'static>, channels: Itc) {
     info!("Starting Command Runner Thread");
     let mut buf: [u8; 100] = [0; 100];
     let _result = connection_handle.read_until(&mut buf, b'\n');
