62b766e6e1d0365f46fbff356896ad1750ebc506
Jorge Gomez <jgomez101609@gmail.com>
2025-07-22 20:47:43 -0400

    began adding command processing code

--- src/threads/handler.rs
+++ src/threads/handler.rs
@@ -1,8 +1,9 @@
-use crate::interaction::command::{Command, parse_scenario};
+use crate::interaction::command::{self, Command, parse_scenario};
 use crate::interaction::config::Config;
+use crate::threads::Itc;
 use log::{info, warn};
 
-pub fn thread(config: Config) {
+pub fn thread(config: Config, runner_channels: Itc) {
     info!("Starting Scenario Handler Thread");
     for scenario in config.scenarios {
         if !scenario.is_file() {
@@ -10,7 +11,24 @@ pub fn thread(config: Config) {
             continue;
         }
 
-        let scenario_commands = parse_scenario(scenario);
-        print!("Here: {:?}", scenario_commands);
+        let scenario_commands = match parse_scenario(&scenario) {
+            Ok(commands) => commands,
+            Err(_) => {
+                warn!("{} could not be parsed, skipping", scenario.display());
+                continue;
+            }
+        };
+
+        for command in scenario_commands {
+            match command.command {
+                command::Type::Standard {
+                    send,
+                    expect_prefix,
+                    expect_exact,
+                    timeout,
+                    delay,
+                } => {}
+            };
+        }
     }
 }
