dd35b84bf59e008910aa0f1d5f0ea1d329ecfd8d
Jorge Gomez <jgomez101609@gmail.com>
2025-07-22 20:45:57 -0400

    added itc.rs

---
+++ src/threads/itc.rs
@@ -0,0 +1,27 @@
+use chrono::{DateTime, Local};
+use std::sync::mpsc::{Receiver, Sender};
+
+pub enum Messages {
+    SendData {
+        data: Vec<u8>,
+    },
+    DataReceived {
+        timestamp: DateTime<Local>,
+        data: Vec<u8>,
+    },
+    StopRunning,
+}
+
+pub struct Itc {
+    send_channel: Sender<Messages>,
+    receive_channel: Receiver<Messages>,
+}
+
+impl Itc {
+    pub fn new(send_channel: Sender<Messages>, receive_channel: Receiver<Messages>) -> Self {
+        Self {
+            send_channel,
+            receive_channel,
+        }
+    }
+}
